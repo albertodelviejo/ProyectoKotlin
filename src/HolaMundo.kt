b77386fc0ff71b3bfb2a4d830226f45b4195f971
albertodelviejo <albertodelviejosanchez@gmail.com>
2020-07-30 15:08:04 +0200

    Clase Operadores en Kotlin

---
+++ src/HolaMundo.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>){
+    println("Hola mundo")
+    println(1+1)
+    println(3-1)
+    println(2*2)
+    println(4/2)
+    println(7%2)
+
+    val a = 4
+    val b = 2
+}
