b89024d861cb06c1399165c139175fdffdb3fea9
albertodelviejo <albertodelviejosanchez@gmail.com>
2020-07-30 15:35:12 +0200

    Clase de val, var y const

--- src/HolaMundo.kt
+++ src/HolaMundo.kt
@@ -1,3 +1,10 @@
+//La variable global tiene que ser unchangeable para no contaminar las funciones puras (De otro modo se podría modificar y alterar todo)
+
+const val N = "NAME" // const siempre sera para variables globales
+//var n = 3 --> Mala práctica por que la variable global puede ser cambiada al ser changeable
+
+//Funciones puras 
+
 fun main(args: Array<String>){
     println("Hola mundo")
     println(1+1)
@@ -6,7 +13,7 @@ fun main(args: Array<String>){
     println(4/2)
     println(7%2)
 
-    val a = 4
+    val a = 4   //val se utiliza para variables locales 
     val b = 2
 
     println(a.plus(b))
@@ -14,4 +21,5 @@ fun main(args: Array<String>){
     println(a.times(b))
     println(a.div(b))
 
+    println(N)
 }
